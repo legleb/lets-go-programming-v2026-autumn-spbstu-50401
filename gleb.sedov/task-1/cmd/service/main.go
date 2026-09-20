37516428a938a86f49667d3367e8040a3d81a65b
legleb <sedovgleb2570@gmail.com>
2026-09-20 12:02:21 +0300

    [TASK-1] Complete task

--- gleb.sedov/task-1/cmd/service/main.go
+++ gleb.sedov/task-1/cmd/service/main.go
@@ -1,4 +1,39 @@
 package main
 
+import "fmt"
+
 func main() {
+	var a, b int
+	var op string
+	_, err1 := fmt.Scan(&a)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err2 := fmt.Scan(&b)
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err3 := fmt.Scan(&op)
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+	}
+
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
